07ff98aadbbd0837a2c231ccc14a299300f5f987
Adnane <nanitosse@gmail.com>
2023-09-29 21:56:48 -0400

    node-express

---
+++ node-express/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+const morgan = require('morgan')
+// const cors= require('cors');
+// const calisthenicsRouter = require('./routes/calisthenicsRouter');
+// const nutritionRouter = require('./routes/nutritionRouter');
+// const weightRouter = require('./routes/weightRouter');
+
+const app = express();
+const hostname = 'localhost';
+const port = 3000;
+
+
+app.use(bodyParser.json());
+// app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// app.use('/calisthenics', calisthenicsRouter);
+// app.use('/nutritions', nutritionRouter);
+// app.use('/weights',weightRouter);
+
+app.use((req,res)=>{
+  console.log(req.headers);
+    res.statusCode=200,
+    res.setHeader('Content-Type', 'application/json')
+    res.end("AppSport")
+
+})
+
+
+
+
+
+
+app.listen(port,hostname, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
